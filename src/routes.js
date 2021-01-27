05e4c2e64d0e2c40170da7ef117fb0e83ec99a9b
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-26 23:47:27 -0300

    :construction: add route get and post

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const routes = express.Router();
 
+const UserController = require("./controllers/UsersController");
+
+routes.get("/users", UserController.list);
+routes.post("/users", UserController.store);
+
 module.exports = routes;

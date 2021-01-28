00f790672516a98e37a597c1390992e5597998fe
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-28 09:25:46 -0300

    :construction: route to create address to a user

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const routes = express.Router();
 
 const UserController = require("./controllers/UsersController");
+const AddressController = require("./controllers/AddressController");
 
 routes.get("/users", UserController.list);
 routes.post("/users", UserController.store);
+
+routes.get("/addresses", AddressController.list);
+routes.post("/users/:user_id/addresses", AddressController.store);
 
 module.exports = routes;

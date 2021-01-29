7b4542e1a8944c621722f3a1c240f6c06c98ff10
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-28 21:40:42 -0300

    :construction: add address associate with user model

--- src/models/Address.js
+++ src/models/Address.js
@@ -12,8 +12,19 @@ const Address = database.define(
     number: {
       type: DataTypes.INTEGER,
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
   },
   {}
 );
+
+Address.associate = (models) => {
+  Address.belongsTo(models.User, { foreignKey: "user_id", as: "user" });
+};
 
 module.exports = Address;

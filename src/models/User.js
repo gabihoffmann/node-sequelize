f04240fea5d31c5f8060dfa6e55bad91b723f26c
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-31 20:24:07 -0300

    🤷‍♀️  need change to work the user-techs association

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { database, DataTypes } = require("../database/index");
 const Address = require("./Address");
+const Tech = require("./Tech");
 
 const User = database.define(
   "users",
@@ -15,13 +16,18 @@ const User = database.define(
 );
 
 User.hasMany(Address, { foreignKey: "user_id", as: "addresses" });
+User.belongsToMany(Tech, {
+  foreignKey: "user_id",
+  through: "user_techs",
+  as: "techs",
+});
 
-User.associate = (models) => {
-  User.belongsToMany(models.Tech, {
-    foreignKey: "user_id",
-    through: "user_techs",
-    as: "techs",
-  });
-};
+// User.associate = (models) => {
+//   User.belongsToMany(models.Tech, {
+//     foreignKey: "user_id",
+//     through: "user_techs",
+//     as: "techs",
+//   });
+// };
 
 module.exports = User;

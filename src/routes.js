cf937609a2c28fe9ec104283535334e55fd36b40
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-31 19:09:52 -0300

    🚧  adding routes for technologies resource

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,15 @@ const routes = express.Router();
 
 const UserController = require("./controllers/UsersController");
 const AddressController = require("./controllers/AddressController");
+const TechController = require("./controllers/TechController");
 
 routes.get("/users", UserController.list);
 routes.post("/users", UserController.store);
 
 routes.get("/users/:user_id/addresses", AddressController.list);
 routes.post("/users/:user_id/addresses", AddressController.store);
+
+routes.get("/users/:user_id/techs", TechController.list);
+routes.post("/users/:user_id/techs", TechController.store);
 
 module.exports = routes;

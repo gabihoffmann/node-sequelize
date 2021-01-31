739574fe5ae5d4b607f79bb26cbfc0068e3cc66e
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-31 18:58:23 -0300

    🚧  Add the N:M association whit Tech

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { database, DataTypes } = require("../database/index");
 const Address = require("./Address");
+const Tech = require("./Tech");
 
 const User = database.define(
   "User",
@@ -15,6 +16,14 @@ const User = database.define(
 );
 
 User.hasMany(Address, { foreignKey: "user_id", as: "addresses" });
+
+User.associate = (models) => {
+  User.belongsToMany(models.Tech, {
+    foreignKey: "user_id",
+    through: "user_techs",
+    as: "techs",
+  });
+};
 
 // User.associate = (models) => {
 //   User.hasMany(models.Address, { foreignKey: "user_id", as: "addresses" });

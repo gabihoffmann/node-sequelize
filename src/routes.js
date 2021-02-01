a807c2171d0ef3ec9a4f24c30f80082e03649160
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-31 21:28:18 -0300

    🚧  add route to report

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const routes = express.Router();
 
-const UserController = require("./controllers/UsersController");
 const AddressController = require("./controllers/AddressController");
+const ReportController = require("./controllers/ReportController");
 const TechController = require("./controllers/TechController");
+const UserController = require("./controllers/UsersController");
 
 routes.get("/users", UserController.list);
 routes.post("/users", UserController.store);
@@ -14,5 +15,7 @@ routes.post("/users/:user_id/addresses", AddressController.store);
 routes.get("/users/:user_id/techs", TechController.list);
 routes.post("/users/:user_id/techs", TechController.store);
 routes.delete("/users/:user_id/techs", TechController.delete);
+
+routes.get("/report", ReportController.show);
 
 module.exports = routes;
